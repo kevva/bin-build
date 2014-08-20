8aebe3e098a941e0f028eb3fd2a54b7c4f7fb6e7
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-21 01:25:34 +0200

    Use ava for testing

--- test.js
+++ test.js
@@ -1,28 +1,49 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
 var BinBuild = require('./');
 var fs = require('fs');
 var path = require('path');
-var rm = require('rimraf');
+var test = require('ava');
 
-describe('BinBuild()', function () {
-    it('should download and build source', function (cb) {
-        var build = new BinBuild();
-        var tmp = path.join(__dirname, 'tmp');
+test('expose a constructor', function (t) {
+    t.plan(1);
+    t.assert(typeof BinBuild === 'function');
+});
 
-        build
-            .src('http://www.lcdf.org/gifsicle/gifsicle-1.80.tar.gz')
-            .cmd('./configure --disable-gifview --disable-gifdiff --prefix="' + tmp + '" --bindir="' + tmp + '"')
-            .cmd('make install')
-            .build(function (err) {
-                assert(!err);
+test('set source file', function (t) {
+    t.plan(1);
 
-                fs.exists(path.join(tmp, 'gifsicle'), function (exists) {
-                    assert(exists);
-                    rm(tmp, cb);
-                });
-            });
+    var build = new BinBuild()
+        .src('src.tar.gz');
+
+    t.assert(build._src === 'src.tar.gz');
+});
+
+test('add commands to run', function (t) {
+    t.plan(2);
+
+    var build = new BinBuild()
+        .cmd('make')
+        .cmd('make install');
+
+    t.assert(build._cmd[0] === 'make');
+    t.assert(build._cmd[1] === 'make install');
+});
+
+test('should download and build source', function (t) {
+    t.plan(2);
+
+    var tmp = path.join(__dirname, 'tmp');
+    var build = new BinBuild()
+        .src('http://www.lcdf.org/gifsicle/gifsicle-1.80.tar.gz')
+        .cmd('./configure --disable-gifview --disable-gifdiff --prefix="' + tmp + '" --bindir="' + tmp + '"')
+        .cmd('make install');
+
+    build.build(function (err) {
+        t.assert(!err);
+
+        fs.exists(path.join(tmp, 'gifsicle'), function (exists) {
+            t.assert(exists);
+        });
     });
 });
